f1eda146345a7b2e7f87dbdc496e980d1d7c6330
Hassan Shahid <hassan2014201554@gmail.com>
2020-08-14 18:02:33 +0300

    hassan main.Dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,451 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 
-void main() {
-  runApp(MyApp());
+void main() => runApp(MyApp());
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class _MyAppState extends State<MyApp> {
+  bool _isInChannel = false;
+  final _infoStrings = <String>[];
+
+  TextEditingController _controller = TextEditingController(text: "0");
+  static final _sessions = List<VideoSession>();
+  String dropdownValue = 'Off';
+
+  final List<String> voices = [
+    'Off',
+    'Oldman',
+    'BabyBoy',
+    'BabyGirl',
+    'Zhubajie',
+    'Ethereal',
+    'Hulk'
+  ];
+
+  /// remote user list
+  final _remoteUsers = List<int>();
+
+  @override
+  void initState() {
+    super.initState();
+
+    _initAgoraRtcEngine();
+    _addAgoraEventHandlers();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Agora Flutter SDK'),
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Container(height: 320, child: _viewRows()),
+              TextField(
+                controller: _controller,
+              ),
+              OutlineButton(
+                child: Text(_isInChannel ? 'Leave Channel' : 'Join Channel',
+                    style: textStyle),
+                onPressed: _toggleChannel,
+              ),
+              MaterialButton(onPressed: _mute1),
+              Container(height: 100, child: _voiceDropdown()),
+              Expanded(child: Container(child: _buildInfoList())),
+            ],
+          ),
+        ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
-}
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
+  Widget _voiceDropdown() {
+    return Scaffold(
+      body: Center(
+        child: DropdownButton<String>(
+          value: dropdownValue,
+          onChanged: (String newValue) {
+            setState(() {
+              dropdownValue = newValue;
+              VoiceChanger voice =
+              VoiceChanger.values[(voices.indexOf(dropdownValue))];
+              AgoraRtcEngine.setLocalVoiceChanger(voice);
+            });
+          },
+          items: voices.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
 
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  Future<void> _mute1() async {
+    int x = int.parse(_controller.text) * -1;
+    AgoraRtcEngine.muteRemoteAudioStream(x, false);
+  }
 
-  final String title;
+  Future<void> _initAgoraRtcEngine() async {
+    AgoraRtcEngine.create('2bd96b8c4aa74c648b5a4d225bbce8ba');
 
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+    AgoraRtcEngine.enableAudio();
+    AgoraRtcEngine.setEnableSpeakerphone(true);
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+//    AgoraRtcEngine.enableSoundPositionIndication(true);
+    AgoraRtcEngine.setDefaultMuteAllRemoteAudioStreams(true);
+//    AgoraRtcEngine.muteRemoteAudioStream(987654321, false);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+
+//    AgoraRtcEngine.enableDualStreamMode(true);
+//    AgoraRtcEngine.enableLocalAudio(true);
+//    // AgoraRtcEngine.setParameters('{\"che.video.lowBitRateStreamParameter\":{\"width\":320,\"height\":180,\"frameRate\":15,\"bitRate\":140}}');
+    AgoraRtcEngine.setChannelProfile(ChannelProfile.Communication);
+//
+//    VideoEncoderConfiguration config = VideoEncoderConfiguration();
+//    config.orientationMode = VideoOutputOrientationMode.FixedPortrait;
+//    AgoraRtcEngine.setVideoEncoderConfiguration(config);
+  }
+
+  void _addAgoraEventHandlers() {
+    AgoraRtcEngine.onJoinChannelSuccess =
+        (String channel, int uid, int elapsed) {
+      setState(() {
+        String info = 'onJoinChannel: ' + channel + ', uid: ' + uid.toString();
+        _infoStrings.add(info);
+      });
+    };
+
+    AgoraRtcEngine.onLeaveChannel = () {
+      setState(() {
+        _infoStrings.add('onLeaveChannel');
+        _remoteUsers.clear();
+      });
+    };
+
+    AgoraRtcEngine.onUserJoined = (int uid, int elapsed) {
+      setState(() {
+        String info = 'userJoined: ' + uid.toString();
+        _infoStrings.add(info);
+        _remoteUsers.add(uid);
+      });
+    };
+
+    AgoraRtcEngine.onUserOffline = (int uid, int reason) {
+      setState(() {
+        String info = 'userOffline: ' + uid.toString();
+        _infoStrings.add(info);
+        _remoteUsers.remove(uid);
+      });
+    };
+
+    AgoraRtcEngine.onFirstRemoteVideoFrame =
+        (int uid, int width, int height, int elapsed) {
+      setState(() {
+        String info = 'firstRemoteVideo: ' +
+            uid.toString() +
+            ' ' +
+            width.toString() +
+            'x' +
+            height.toString();
+        _infoStrings.add(info);
+      });
+    };
+  }
+
+  void _toggleChannel() {
+    setState(() async {
+      if (_isInChannel) {
+        _isInChannel = false;
+        await AgoraRtcEngine.leaveChannel();
+        await AgoraRtcEngine.stopPreview();
+      } else {
+        _isInChannel = true;
+        await AgoraRtcEngine.startPreview();
+        await AgoraRtcEngine.joinChannel(null, 'flutter', null, 0);
+      }
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+  Widget _viewRows() {
+    return Row(
+      children: <Widget>[
+        for (final widget in _renderWidget)
+          Expanded(
+            child: Container(
+              child: widget,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+          )
+      ],
+    );
+  }
+
+  Iterable<Widget> get _renderWidget sync* {
+    yield AgoraRenderWidget(0, local: true, preview: false);
+
+    for (final uid in _remoteUsers) {
+      yield AgoraRenderWidget(uid);
+    }
+  }
+
+  VideoSession _getVideoSession(int uid) {
+    return _sessions.firstWhere((session) {
+      return session.uid == uid;
+    });
+  }
+
+  List<Widget> _getRenderViews() {
+    return _sessions.map((session) => session.view).toList();
+  }
+
+  static TextStyle textStyle = TextStyle(fontSize: 18, color: Colors.blue);
+
+  Widget _buildInfoList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(8.0),
+      itemExtent: 24,
+      itemBuilder: (context, i) {
+        return ListTile(
+          title: Text(_infoStrings[i]),
+        );
+      },
+      itemCount: _infoStrings.length,
     );
   }
 }
+
+class VideoSession {
+  int uid;
+  Widget view;
+  int viewId;
+
+  VideoSession(int uid, Widget view) {
+    this.uid = uid;
+    this.view = view;
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// p ------------------------------------------------------------ ///
+//void main() {
+//  runApp(MyApp());
+//}
+//
+//class MyApp extends StatelessWidget {
+//  // This widget is the root of your application.
+//  @override
+//  Widget build(BuildContext context) {
+//    return MaterialApp(
+//      title: 'Flutter Demo',
+//      home: MyHomePage(),
+//    );
+//  }
+//}
+//
+//class MyHomePage extends StatefulWidget {
+//
+//  @override
+//  _MyHomePageState createState() => _MyHomePageState();
+//}
+//
+//class _MyHomePageState extends State<MyHomePage> {
+//  @override
+//  Widget build(BuildContext context) {
+//    return Scaffold(
+//      appBar: AppBar(
+//        title: Text("ECO APP"),
+//        centerTitle: true,
+//        backgroundColor: Colors.pinkAccent.shade100,
+//      ),
+//      body: Center(
+//        child: ListView(
+//          children: <Widget>[
+//            ListTile(
+//              leading: CircleAvatar(
+//            radius: 30,
+//            backgroundImage: NetworkImage("https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Flag_of_Iraq.svg/125px-Flag_of_Iraq.svg.png")
+//        ),
+//              title: Text("العراق"),
+//              subtitle:
+//                   Row(
+//                children: <Widget>[
+//                  SizedBox(width: 20),
+//                  Row(
+//                    children: <Widget>[
+//                      Icon(Icons.group),
+//                      SizedBox(width: 10),
+//                      Text("800"),
+//
+//
+//                    ],
+//                  ),
+//                  SizedBox(width: 30),
+//                  Row(
+//                    children: <Widget>[
+//                      Icon(Icons.message),
+//                      SizedBox(width: 10),
+//                      Text("10"),
+//
+//
+//                    ],
+//                  ),
+//
+//                ],
+//              ),
+//            ),
+//
+//
+//            ListTile(
+//              leading: CircleAvatar(
+//                  radius: 30,
+//                  backgroundImage: NetworkImage("https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/125px-Flag_of_Egypt.svg.png")
+//              ),
+//              title: Text("مصر"),
+//              subtitle:
+//              Row(
+//                children: <Widget>[
+//                  SizedBox(width: 20),
+//                  Row(
+//                    children: <Widget>[
+//                      Icon(Icons.group),
+//                      SizedBox(width: 10),
+//                      Text("300"),
+//
+//
+//                    ],
+//                  ),
+//                  SizedBox(width: 30),
+//                  Row(
+//                    children: <Widget>[
+//                      Icon(Icons.message),
+//                      SizedBox(width: 10),
+//                      Text("8"),
+//
+//
+//                    ],
+//                  ),
+//
+//                ],
+//              ),
+//            ),
+//
+//
+//
+//            ListTile(
+//              leading: CircleAvatar(
+//                  radius: 30,
+//                  backgroundImage: NetworkImage("https://upload.wikimedia.org/wikipedia/commons/thumb/5/53/Flag_of_Syria.svg/125px-Flag_of_Syria.svg.png")
+//              ),
+//              title: Text("سوريا"),
+//              subtitle:
+//              Row(
+//                children: <Widget>[
+//                  SizedBox(width: 20),
+//                  Row(
+//                    children: <Widget>[
+//                      Icon(Icons.group),
+//                      SizedBox(width: 10),
+//                      Text("600"),
+//
+//
+//                    ],
+//                  ),
+//                  SizedBox(width: 30),
+//                  Row(
+//                    children: <Widget>[
+//                      Icon(Icons.message),
+//                      SizedBox(width: 10),
+//                      Text("11"),
+//
+//
+//                    ],
+//                  ),
+//
+//                ],
+//              ),
+//            ),
+//
+//
+//            ListTile(
+//              leading: CircleAvatar(
+//                  radius: 30,
+//                  backgroundImage: NetworkImage("https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Flag_of_Saudi_Arabia.svg/125px-Flag_of_Saudi_Arabia.svg.png")
+//              ),
+//              title: Text("السعودية"),
+//              subtitle:
+//              Row(
+//                children: <Widget>[
+//                  SizedBox(width: 20),
+//                  Row(
+//                    children: <Widget>[
+//                      Icon(Icons.group),
+//                      SizedBox(width: 10),
+//                      Text("760"),
+//
+//
+//                    ],
+//                  ),
+//                  SizedBox(width: 30),
+//                  Row(
+//                    children: <Widget>[
+//                      Icon(Icons.message),
+//                      SizedBox(width: 10),
+//                      Text("12"),
+//
+//
+//                    ],
+//                  ),
+//
+//                ],
+//              ),
+//            ),
+//
+//          ],
+//        ),
+//      ),
+//      bottomNavigationBar: BottomNavigationBar(
+//        backgroundColor: Colors.pinkAccent.shade100,
+//        items: const <BottomNavigationBarItem>[
+//          BottomNavigationBarItem(
+//            icon: Icon(Icons.home),
+//            title: Text('الغرف'),
+//          ),
+//          BottomNavigationBarItem(
+//            icon: Icon(Icons.favorite),
+//            title: Text('المفضلة'),
+//          ),
+//          BottomNavigationBarItem(
+//            icon: Icon(Icons.search),
+//            title: Text('بحث'),
+//          ),
+//        ],
+//        currentIndex: 0,
+//        selectedItemColor: Colors.white,
+//        onTap: (d) => {},
+//      ),
+//    );
+//  }
+//}
