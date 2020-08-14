e04a4330b01e5ac8f01cfce966f6677c1a32329b
Ameer Ali <johncena10021@gmail.com>
2020-08-15 00:00:53 +0300

    build search design

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -9,7 +9,34 @@ class _SearchState extends State<Search> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text('Search Screen'),),
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        child: Stack(
+          children: [
+            TextFormField(
+              textAlign: TextAlign.center,
+              decoration:
+                  InputDecoration(hintText: 'ادخل اسم الغرفة او رقم الغرفة'),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Row(
+                children: <Widget>[
+                  SizedBox(width: 20),
+                  Icon(Icons.group),
+                  SizedBox(width: 10),
+                  Text('202'),
+                  SizedBox(width: 30),
+                  Icon(Icons.message),
+                  SizedBox(width: 10),
+                  Text('112'),
+                ],
+              ),
+            ),
+
+          ],
+        ),
+      ),
     );
   }
 }
