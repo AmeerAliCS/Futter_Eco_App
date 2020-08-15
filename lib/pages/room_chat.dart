ea9f843fc236e709b49e229e9f8d62b042aed8bf
Hassan Shahid <hassan2014201554@gmail.com>
2020-08-15 23:56:27 +0300

    chat Messages Done

--- lib/pages/room_chat.dart
+++ lib/pages/room_chat.dart
@@ -1,7 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class RoomChat extends StatefulWidget {
-
   RoomChat({@required this.name, @required this.uid});
   final String name;
   final String uid;
@@ -11,7 +11,6 @@ class RoomChat extends StatefulWidget {
 }
 
 class _RoomChatState extends State<RoomChat> {
-
   String messageText;
   TextEditingController messageController = TextEditingController();
   final DateTime timestamp = DateTime.now();
@@ -24,17 +23,35 @@ class _RoomChatState extends State<RoomChat> {
         title: Text('Room Chat'),
         centerTitle: true,
       ),
-
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Expanded(
-            child: ListView(
-              children: [
-                Text(widget.name),
-                Text(widget.uid),
-              ],
+            child: StreamBuilder(
+              stream: Firestore.instance
+                  .collection("iraq")
+                  .document('najaf')
+                  .collection('messages')
+                  .orderBy('date')
+                  .snapshots(),
+              builder: (context, snapshot) {
+                return !snapshot.hasData
+                    ? Center(child: Text('انتظر رجاءا'))
+                    : ListView.builder(
+                        itemCount: snapshot.data.documents.length,
+                        itemBuilder: (context, index) {
+                          DocumentSnapshot message =
+                              snapshot.data.documents[index];
+                          bool _isSamePerson = message['sender'] == widget.name;
+                          return Bubble(
+                              message: message['text'],
+                              time: "12:00",
+                              isMe: _isSamePerson,
+                              delivered: false);
+                        },
+                      );
+              },
             ),
           ),
           Container(
@@ -48,8 +65,17 @@ class _RoomChatState extends State<RoomChat> {
               children: <Widget>[
                 FlatButton(
                   onPressed: () {
+                    Firestore.instance
+                        .collection('iraq')
+                        .document('najaf')
+                        .collection('messages')
+                        .document()
+                        .setData({
+                      'sender': widget.name,
+                      'text': messageController.text,
+                      'date': DateTime.now()
+                    });
                     messageController.clear();
-                    // save message to firestore
                   },
                   child: Text(
                     'Send',
@@ -67,19 +93,92 @@ class _RoomChatState extends State<RoomChat> {
                       messageText = value;
                     },
                     decoration: InputDecoration(
-                      contentPadding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                      contentPadding: EdgeInsets.symmetric(
+                          vertical: 10.0, horizontal: 20.0),
                       hintText: 'اكتب رسالتك هنا...',
                       border: InputBorder.none,
                     ),
                   ),
                 ),
-
               ],
             ),
           ),
         ],
       ),
+    );
+  }
+}
 
+class Bubble extends StatelessWidget {
+  Bubble({this.message, this.time, this.delivered, this.isMe});
+
+  final String message, time;
+  final delivered, isMe;
+
+  @override
+  Widget build(BuildContext context) {
+    final bg = isMe ? Colors.white : Colors.pinkAccent.shade100;
+    final align = isMe ? CrossAxisAlignment.start : CrossAxisAlignment.end;
+    final icon = delivered ? Icons.done_all : Icons.done;
+    final radius = isMe
+        ? BorderRadius.only(
+            topLeft: Radius.circular(5.0),
+            bottomLeft: Radius.circular(5.0),
+            bottomRight: Radius.circular(10.0),
+          )
+        : BorderRadius.only(
+            topRight: Radius.circular(5.0),
+            bottomLeft: Radius.circular(10.0),
+            bottomRight: Radius.circular(5.0),
+          );
+    return Column(
+      crossAxisAlignment: align,
+      children: <Widget>[
+        Container(
+          margin: const EdgeInsets.all(3.0),
+          padding: const EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            boxShadow: [
+              BoxShadow(
+                  blurRadius: .5,
+                  spreadRadius: 1.0,
+                  color: Colors.black.withOpacity(.12))
+            ],
+            color: bg,
+            borderRadius: radius,
+          ),
+          child: Stack(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(right: 48.0),
+                child: Padding(padding: EdgeInsets.only(bottom: 6),
+                child: Text(message)),
+              ),
+              Positioned(
+                bottom: 0.0,
+                right: 0.0,
+                child: Row(
+                  children: <Widget>[
+                    Text(time,
+                        style: TextStyle(
+                          color: Colors.black38,
+                          fontSize: 10.0,
+                        )),
+                    SizedBox(width: 5),
+                    Text(
+                      'Hassan',
+                      style: TextStyle(
+                        fontSize: 12,
+                        color: Colors.black38,
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
