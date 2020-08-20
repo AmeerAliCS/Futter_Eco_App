fe149d3a7e731e7915452bda703e6f91c3579abe
hassanalking <hassana2014201554@gmail.com>
2020-08-20 19:53:02 +0300

    test mic7

--- lib/main.dart
+++ lib/main.dart
@@ -3,226 +3,226 @@ import 'package:eco_app/pages/home.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
-
-void main() => runApp(MaterialApp(
-    localizationsDelegates: [
+void main() => runApp(MaterialApp(localizationsDelegates: [
       // ... app-specific localization delegate[s] here
       GlobalMaterialLocalizations.delegate,
       GlobalWidgetsLocalizations.delegate,
       GlobalCupertinoLocalizations.delegate,
-    ],
-    supportedLocales: [
+    ], supportedLocales: [
       const Locale('ar', 'AE')
-    ],
-    debugShowCheckedModeBanner: false,
-    home: MyApp()));
-
-
-
-
-
-
-
-
-
-
-
-
+    ], debugShowCheckedModeBanner: false, home: MyApp()));
 
 class MyApp extends StatefulWidget {
- @override
- _MyAppState createState() => _MyAppState();
+  @override
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
- bool _isInChannel = false;
- final _infoStrings = <String>[];
+  bool _isInChannel = false;
+  final _infoStrings = <String>[];
 
- TextEditingController _controller = TextEditingController(text: "0");
+  TextEditingController _controller = TextEditingController(text: "0");
 //  static final _sessions = List<VideoSession>();
- String dropdownValue = 'Off';
+  String dropdownValue = 'Off';
 
- final List<String> voices = [
-   'Off',
-   'Oldman',
-   'BabyBoy',
-   'BabyGirl',
-   'Zhubajie',
-   'Ethereal',
-   'Hulk'
- ];
+  final List<String> voices = [
+    'Off',
+    'Oldman',
+    'BabyBoy',
+    'BabyGirl',
+    'Zhubajie',
+    'Ethereal',
+    'Hulk'
+  ];
 
- /// remote user list
- final _remoteUsers = List<int>();
+  /// remote user list
+  final _remoteUsers = List<int>();
 
- @override
- void initState() {
-   super.initState();
+  @override
+  void initState() {
+    super.initState();
 
-   _initAgoraRtcEngine();
-   _addAgoraEventHandlers();
- }
+    _initAgoraRtcEngine();
+    _addAgoraEventHandlers();
+  }
 
- @override
- Widget build(BuildContext context) {
-   return MaterialApp(
-     home: Scaffold(
-       appBar: AppBar(
-         title: Text('Eco App'),
-       ),
-       body: Container(
-         child: Column(
-           children: [
-             Container(height: 320, child: _viewRows()),
-             TextField(
-               controller: _controller,
-             ),
-             OutlineButton(
-               child: Text(_isInChannel ? 'Leave Channel' : 'Join Channel',
-                   style: textStyle),
-               onPressed: _toggleChannel,
-             ),
-             MaterialButton(onPressed: _mute1, child: Text('Mute Id')),
-             Container(height: 100, child: _voiceDropdown()),
-             Expanded(child: Container(child: _buildInfoList())),
-           ],
-         ),
-       ),
-     ),
-   );
- }
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Eco App'),
+        ),
+        body: Container(
+          child: Column(
+            children: [
+              Container(height: 320, child: _viewRows()),
+              TextField(
+                controller: _controller,
+              ),
+              OutlineButton(
+                child: Text(_isInChannel ? 'Leave Channel' : 'Join Channel',
+                    style: textStyle),
+                onPressed: _toggleChannel,
+              ),
+              MaterialButton(onPressed: _mute1, child: Text('Mute Id')),
+              Container(height: 100, child: _voiceDropdown()),
+              Expanded(child: Container(child: _buildInfoList())),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
- Widget _voiceDropdown() {
-   return Scaffold(
-     body: Center(
-       child: DropdownButton<String>(
-         value: dropdownValue,
-         onChanged: (String newValue) {
-           setState(() {
-             dropdownValue = newValue;
-             VoiceChanger voice =
-             VoiceChanger.values[(voices.indexOf(dropdownValue))];
-             AgoraRtcEngine.setLocalVoiceChanger(voice);
-           });
-         },
-         items: voices.map<DropdownMenuItem<String>>((String value) {
-           return DropdownMenuItem<String>(
-             value: value,
-             child: Text(value),
-           );
-         }).toList(),
-       ),
-     ),
-   );
- }
+  Widget _voiceDropdown() {
+    return Scaffold(
+      body: Center(
+        child: DropdownButton<String>(
+          value: dropdownValue,
+          onChanged: (String newValue) {
+            setState(() {
+              dropdownValue = newValue;
+              VoiceChanger voice =
+                  VoiceChanger.values[(voices.indexOf(dropdownValue))];
+              AgoraRtcEngine.setLocalVoiceChanger(voice);
+            });
+          },
+          items: voices.map<DropdownMenuItem<String>>((String value) {
+            return DropdownMenuItem<String>(
+              value: value,
+              child: Text(value),
+            );
+          }).toList(),
+        ),
+      ),
+    );
+  }
 
+  Future<void> _mute1() async {
+    int x = int.parse(_controller.text);
+    AgoraRtcEngine.muteAllRemoteAudioStreams(true);
+    AgoraRtcEngine.muteRemoteAudioStream(x, false);
+  }
 
- Future<void> _mute1() async {
-   int x = int.parse(_controller.text);
-   AgoraRtcEngine.muteRemoteAudioStream(x, false);
- }
+  Future<void> _initAgoraRtcEngine() async {
+    AgoraRtcEngine.create('2bd96b8c4aa74c648b5a4d225bbce8ba');
 
- Future<void> _initAgoraRtcEngine() async {
-   AgoraRtcEngine.create('2bd96b8c4aa74c648b5a4d225bbce8ba');
+    AgoraRtcEngine.enableAudio();
+    AgoraRtcEngine.setDefaultAudioRouteToSpeaker(true);
+    AgoraRtcEngine.setEnableSpeakerphone(true);
 
-   AgoraRtcEngine.enableAudio();
-   AgoraRtcEngine.setEnableSpeakerphone(true);
-
+    AgoraRtcEngine.isSpeakerphoneEnabled()
+        .then((value) => print('is Speaker: $value'));
 //    AgoraRtcEngine.enableSoundPositionIndication(true);
-   AgoraRtcEngine.setDefaultMuteAllRemoteAudioStreams(true);
+    //  AgoraRtcEngine.setDefaultMuteAllRemoteAudioStreams(true);
 //    AgoraRtcEngine.muteRemoteAudioStream(987654321, false);
-
 
 //    AgoraRtcEngine.enableDualStreamMode(true);
 //    AgoraRtcEngine.enableLocalAudio(true);
 //    // AgoraRtcEngine.setParameters('{\"che.video.lowBitRateStreamParameter\":{\"width\":320,\"height\":180,\"frameRate\":15,\"bitRate\":140}}');
-   AgoraRtcEngine.setChannelProfile(ChannelProfile.Communication);
+    AgoraRtcEngine.setChannelProfile(ChannelProfile.Communication);
 //
 //    VideoEncoderConfiguration config = VideoEncoderConfiguration();
 //    config.orientationMode = VideoOutputOrientationMode.FixedPortrait;
 //    AgoraRtcEngine.setVideoEncoderConfiguration(config);
- }
+  }
 
- void _addAgoraEventHandlers() {
-   AgoraRtcEngine.onJoinChannelSuccess =
-       (String channel, int uid, int elapsed) {
-     setState(() {
-       String info = 'onJoinChannel: ' + channel + ', uid: ' + uid.toString();
-       _infoStrings.add(info);
-     });
-   };
+  void _addAgoraEventHandlers() {
+    AgoraRtcEngine.onJoinChannelSuccess =
+        (String channel, int uid, int elapsed) {
+      AgoraRtcEngine.setDefaultAudioRouteToSpeaker(true);
+      AgoraRtcEngine.setEnableSpeakerphone(true);
 
-   AgoraRtcEngine.onLeaveChannel = () {
-     setState(() {
-       _infoStrings.add('onLeaveChannel');
-       _remoteUsers.clear();
-     });
-   };
+      AgoraRtcEngine.isSpeakerphoneEnabled()
+          .then((value) => print('is Speaker: $value'));
+      setState(() {
+        String info = 'onJoinChannel: ' + channel + ', uid: ' + uid.toString();
+        _infoStrings.add(info);
+      });
+    };
 
-   AgoraRtcEngine.onUserJoined = (int uid, int elapsed) {
-     setState(() {
-       String info = 'userJoined: ' + uid.toString();
-       _infoStrings.add(info);
-       _remoteUsers.add(uid);
-     });
-   };
+    AgoraRtcEngine.onLeaveChannel = () {
+      AgoraRtcEngine.setDefaultAudioRouteToSpeaker(true);
+      AgoraRtcEngine.setEnableSpeakerphone(true);
 
-   AgoraRtcEngine.onUserOffline = (int uid, int reason) {
-     setState(() {
-       String info = 'userOffline: ' + uid.toString();
-       _infoStrings.add(info);
-       _remoteUsers.remove(uid);
-     });
-   };
+      AgoraRtcEngine.isSpeakerphoneEnabled()
+          .then((value) => print('is Speaker: $value'));
+      setState(() {
+        _infoStrings.add('onLeaveChannel');
+        _remoteUsers.clear();
+      });
+    };
 
-   AgoraRtcEngine.onFirstRemoteVideoFrame =
-       (int uid, int width, int height, int elapsed) {
-     setState(() {
-       String info = 'firstRemoteVideo: ' +
-           uid.toString() +
-           ' ' +
-           width.toString() +
-           'x' +
-           height.toString();
-       _infoStrings.add(info);
-     });
-   };
- }
+    AgoraRtcEngine.onUserJoined = (int uid, int elapsed) {
+      AgoraRtcEngine.setDefaultAudioRouteToSpeaker(true);
+      AgoraRtcEngine.setEnableSpeakerphone(true);
 
- void _toggleChannel() {
-   setState(() async {
-     if (_isInChannel) {
-       _isInChannel = false;
-       await AgoraRtcEngine.leaveChannel();
-       await AgoraRtcEngine.stopPreview();
-     } else {
-       _isInChannel = true;
-       await AgoraRtcEngine.startPreview();
-       await AgoraRtcEngine.joinChannel(null, 'flutter', null, 0);
-     }
-   });
- }
+      AgoraRtcEngine.isSpeakerphoneEnabled()
+          .then((value) => print('is Speaker: $value'));
+      setState(() {
+        String info = 'userJoined: ' + uid.toString();
+        _infoStrings.add(info);
+        _remoteUsers.add(uid);
+      });
+    };
 
- Widget _viewRows() {
-   return Row(
-     children: <Widget>[
-       for (final widget in _renderWidget)
-         Expanded(
-           child: Container(
-             child: widget,
-           ),
-         )
-     ],
-   );
- }
+    AgoraRtcEngine.onUserOffline = (int uid, int reason) {
+      setState(() {
+        String info = 'userOffline: ' + uid.toString();
+        _infoStrings.add(info);
+        _remoteUsers.remove(uid);
+      });
+    };
 
- Iterable<Widget> get _renderWidget sync* {
-   yield AgoraRenderWidget(0, local: true, preview: false);
+    AgoraRtcEngine.onFirstRemoteVideoFrame =
+        (int uid, int width, int height, int elapsed) {
+      setState(() {
+        String info = 'firstRemoteVideo: ' +
+            uid.toString() +
+            ' ' +
+            width.toString() +
+            'x' +
+            height.toString();
+        _infoStrings.add(info);
+      });
+    };
+  }
 
-   for (final uid in _remoteUsers) {
-     yield AgoraRenderWidget(uid);
-   }
- }
+  void _toggleChannel() {
+    setState(() async {
+      if (_isInChannel) {
+        _isInChannel = false;
+        await AgoraRtcEngine.leaveChannel();
+        await AgoraRtcEngine.stopPreview();
+      } else {
+        _isInChannel = true;
+        await AgoraRtcEngine.startPreview();
+        await AgoraRtcEngine.joinChannel(null, 'flutter', null, 0);
+      }
+    });
+  }
+
+  Widget _viewRows() {
+    return Row(
+      children: <Widget>[
+        for (final widget in _renderWidget)
+          Expanded(
+            child: Container(
+              child: widget,
+            ),
+          )
+      ],
+    );
+  }
+
+  Iterable<Widget> get _renderWidget sync* {
+    yield AgoraRenderWidget(0, local: true, preview: false);
+
+    for (final uid in _remoteUsers) {
+      yield AgoraRenderWidget(uid);
+    }
+  }
 
 //  VideoSession _getVideoSession(int uid) {
 //    return _sessions.firstWhere((session) {
@@ -234,31 +234,18 @@ class _MyAppState extends State<MyApp> {
 //    return _sessions.map((session) => session.view).toList();
 //  }
 
- static TextStyle textStyle = TextStyle(fontSize: 18, color: Colors.blue);
+  static TextStyle textStyle = TextStyle(fontSize: 18, color: Colors.blue);
 
- Widget _buildInfoList() {
-   return ListView.builder(
-     padding: const EdgeInsets.all(8.0),
-     itemExtent: 24,
-     itemBuilder: (context, i) {
-       return ListTile(
-         title: Text(_infoStrings[i]),
-       );
-     },
-     itemCount: _infoStrings.length,
-   );
- }
+  Widget _buildInfoList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(8.0),
+      itemExtent: 24,
+      itemBuilder: (context, i) {
+        return ListTile(
+          title: Text(_infoStrings[i]),
+        );
+      },
+      itemCount: _infoStrings.length,
+    );
+  }
 }
-
-class VideoSession {
- int uid;
- Widget view;
- int viewId;
-
- VideoSession(int uid, Widget view) {
-   this.uid = uid;
-   this.view = view;
- }
-}
-
-
