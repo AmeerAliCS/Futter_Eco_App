f00b2518a8d57af5051666a0bb6d937669a6ddb5
hassanalking <hassana2014201554@gmail.com>
2020-08-20 16:06:23 +0300

    mic add

--- lib/pages/room_chat.dart
+++ lib/pages/room_chat.dart
@@ -1,3 +1,4 @@
+import 'package:agora_rtc_engine/agora_rtc_engine.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:emoji_picker/emoji_picker.dart';
 import 'package:flutter/material.dart';
@@ -51,10 +52,24 @@ class _RoomChatState extends State<RoomChat> {
 //    });
   }
 
+//Mic 
+
+  bool _isInChannel = false;
+  final _infoStrings = <String>[];
+
+  static final _sessions = List<VideoSession>();
+
+  /// remote user list
+  final _remoteUsers = List<int>();
+
+
+
   @override
   void initState() {
     super.initState();
     getAdmin();
+    _initAgoraRtcEngine();
+    _addAgoraEventHandlers();
   }
 
   @override
@@ -344,6 +359,131 @@ class _RoomChatState extends State<RoomChat> {
       }
 
     }
+
+//Mic Functions
+
+
+Future<void> _initAgoraRtcEngine() async {
+    AgoraRtcEngine.create('2bd96b8c4aa74c648b5a4d225bbce8ba');
+
+    AgoraRtcEngine.enableAudio();
+    AgoraRtcEngine.setEnableSpeakerphone(true);
+    AgoraRtcEngine.muteAllRemoteAudioStreams(true);
+    // AgoraRtcEngine.setParameters('{\"che.video.lowBitRateStreamParameter\":{\"width\":320,\"height\":180,\"frameRate\":15,\"bitRate\":140}}');
+    AgoraRtcEngine.setChannelProfile(ChannelProfile.Communication);
+
+
+    // AgoraRtcEngine.enableVideo();
+
+    // VideoEncoderConfiguration config = VideoEncoderConfiguration();
+    // config.orientationMode = VideoOutputOrientationMode.FixedPortrait;
+    // AgoraRtcEngine.setVideoEncoderConfiguration(config);
+  }
+
+  void _addAgoraEventHandlers() {
+    AgoraRtcEngine.onJoinChannelSuccess =
+        (String channel, int uid, int elapsed) {
+      setState(() {
+        String info = 'onJoinChannel: ' + channel + ', uid: ' + uid.toString();
+        _infoStrings.add(info);
+      });
+    };
+
+    AgoraRtcEngine.onLeaveChannel = () {
+      setState(() {
+        _infoStrings.add('onLeaveChannel');
+        _remoteUsers.clear();
+      });
+    };
+
+    AgoraRtcEngine.onUserJoined = (int uid, int elapsed) {
+      setState(() {
+        String info = 'userJoined: ' + uid.toString();
+        _infoStrings.add(info);
+        _remoteUsers.add(uid);
+      });
+    };
+
+    AgoraRtcEngine.onUserOffline = (int uid, int reason) {
+      setState(() {
+        String info = 'userOffline: ' + uid.toString();
+        _infoStrings.add(info);
+        _remoteUsers.remove(uid);
+      });
+    };
+  }
+
+  void _toggleChannel() {
+    setState(() async {
+      if (_isInChannel) {
+        _isInChannel = false;
+        await AgoraRtcEngine.leaveChannel();
+        await AgoraRtcEngine.stopPreview();
+      } else {
+        _isInChannel = true;
+        await AgoraRtcEngine.startPreview();
+        await AgoraRtcEngine.joinChannel(null, 'flutter', null, 0);
+      }
+    });
+  }
+
+  Widget _viewRows() {
+    return Row(
+      children: <Widget>[
+        for (final widget in _renderWidget)
+          Expanded(
+            child: Container(
+              child: widget,
+            ),
+          )
+      ],
+    );
+  }
+
+  Iterable<Widget> get _renderWidget sync* {
+    yield AgoraRenderWidget(0, local: true, preview: false);
+
+    for (final uid in _remoteUsers) {
+      yield AgoraRenderWidget(uid);
+    }
+  }
+
+  VideoSession _getVideoSession(int uid) {
+    return _sessions.firstWhere((session) {
+      return session.uid == uid;
+    });
+  }
+
+  List<Widget> _getRenderViews() {
+    return _sessions.map((session) => session.view).toList();
+  }
+
+  static TextStyle textStyle = TextStyle(fontSize: 18, color: Colors.blue);
+
+  Widget _buildInfoList() {
+    return ListView.builder(
+      padding: const EdgeInsets.all(8.0),
+      itemExtent: 24,
+      itemBuilder: (context, i) {
+        return ListTile(
+          title: Text(_infoStrings[i]),
+        );
+      },
+      itemCount: _infoStrings.length,
+    );
+  }
+}
+
+class VideoSession {
+  int uid;
+  Widget view;
+  int viewId;
+
+  VideoSession(int uid, Widget view) {
+    this.uid = uid;
+    this.view = view;
+  }
+
 }
 
 class ListOfUsers extends StatelessWidget {
